403ea1c7931716cd601741d30332cd2ec3cfb2c5
Joaquin Osorio <joaquinosorio55@gmail.com>
2024-11-10 22:56:55 -0300

    Minor adjustments in mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div>
       <div className="w-full flex flex-col items-center gap-10 mt-10 mb-1 bg-transparent">
-        <img src={textLogo} alt="textLogo" className='w-2/5' />
-        <p className="font-sfpro bg-transparent">The only exchange where you can trade spot and options on any crypto asset.</p>
+        <img src={textLogo} alt="textLogo" className='w-4/5 sm:w-2/5' />
+        <p className="font-sfpro bg-transparent text-center mx-5">The only exchange where you can trade spot and options on any crypto asset.</p>
       </div>
       <TokensAnimation />
     </div>
